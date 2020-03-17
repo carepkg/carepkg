92b5a426bf791d7e105ac93233827a326fd5afe8
Henry Griffith <hgriffithdev@gmail.com>
2020-03-16 21:12:27 -0700

    required models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,6 +3,10 @@ const Product = require("./product");
 const Review = require("./review");
 const PurchaseProfile = require("./purchaseProfile");
 const OrderProduct = require("./orderProduct");
+const Category = require("./category");
+const PricingHistory = require("./pricingHistory");
+const Order = require("./order");
+const OrderProduct = require("./orderProduct");
 
 // --------- ASSOCIATIONS --------- \\
 
@@ -17,6 +21,9 @@ Review.belongsTo(Product);
 
 Product.belongsToMany(Order, { through: OrderProduct });
 Order.belongsToMany(Product);
+
+Product.belongsToMany(Category);
+Category.belongsToMany(Product);
 
 module.exports = {
   User,
