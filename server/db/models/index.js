b9a84ac88df14968dfd0f0e1756fed99e573dd90
Henry Griffith <hgriffithdev@gmail.com>
2020-04-03 12:55:50 -0700

    updated model associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -14,34 +14,21 @@ const CartLineItem = require("./cartLineItem");
 
 //User can have multiple addresses/shipTo locations saved
 User.hasMany(PurchaseProfile);
-PurchaseProfile.belongsTo(User, { foreignKey: "userId" });
-
-//User can have multiple reviews
 User.hasMany(Review);
-Review.belongsTo(User);
-
-//User can have one cart, but many previous orders
 User.hasMany(Order);
+User.hasMany(LineItem);
+User.hasMany(CartLineItem);
+
+Product.hasMany(Review);
+Product.hasMany(LineItem);
+Product.hasMany(CartLineItem);
+Product.hasMany(PricingHIstory);
+
+PurchaseProfile.belongsTo(User);
+Review.belongsTo(User);
 Order.belongsTo(User);
 
-//Products can have multiple written reviews
-Product.hasMany(Review);
 Review.belongsTo(Product);
-
-//User has many products in their cart. Products can be in many users' carts. Each cartlineitem is a row in the 'cart'
-User.belongsToMany(Product, { through: CartLineItem });
-Product.belongsToMany(User, { through: CartLineItem });
-
-//This is the replacement for OrderProduct
-// Product.belongsToMany(Order, { through: LineItem });
-// Order.belongsToMany(Product, { through: LineItem });
-
-//Product can have many categories it belongs to, categories hold many products
-//Deleted association due to Sequelize Unique Constraint Validation on IDs. Annoying.
-//Instead created ProductCategory table for manual entries
-
-//A product's price changes all the time. PricingHistory is a span of time w/ an associated price
-Product.hasMany(PricingHistory);
 PricingHistory.belongsTo(Product);
 
 module.exports = {
