6d59783633766ffc469426a046deda39894cf2bd
Henry Griffith <hgriffithdev@gmail.com>
2020-04-09 18:14:27 -0700

    borders on pkg and prod imgs

--- src/components/CompanyProfile.js
+++ src/components/CompanyProfile.js
@@ -3,11 +3,27 @@ import { connect } from "react-redux";
 
 const CompanyProfile = props => {
   const { company } = props;
-  console.log(company);
+  const pkgs = company.packages;
   return (
     <div id="company-profile-page">
       <h1>{company.name}</h1>
-      {/* <CoNavBar /> */}
+      {pkgs
+        ? pkgs.map(pkg => {
+            return (
+              <div className="company-profile-pkg">
+                <h3>{pkg.name}</h3>
+                {pkg.packageLineItems.map(item => {
+                  return (
+                    <img
+                      src={item.product.image}
+                      className="company-profile-prod-img"
+                    />
+                  );
+                })}
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 };
