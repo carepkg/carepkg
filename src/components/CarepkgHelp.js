2656efde5735dc2fd6dccf84b70782400c898960
Henry Griffith <hgriffithdev@gmail.com>
2020-04-17 17:45:18 -0700

    CarepkgNewsletter component

--- src/components/CarepkgHelp.js
+++ src/components/CarepkgHelp.js
@@ -7,7 +7,7 @@ const CarepkgHelp = () => {
         <strong>Need Help?</strong>
       </p>
       <p>Chat with our support team 24/7.</p>
-      <button id="chat-btn"> Chat Now</button>
+      <button className="footer-btn"> Chat Now</button>
       <p>
         <strong>Or Call: 1-234-567-8910</strong>
       </p>
--- src/components/CarepkgNewsletter.js
+++ src/components/CarepkgNewsletter.js
@@ -1,7 +1,40 @@
 import React from "react";
 
-const CarepkgNewsletter = () => {
-  return <div className="footer-snippet"></div>;
-};
+class CarepkgNewsletter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: ""
+    };
+  }
+  handleType(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div className="footer-snippet">
+        <p>
+          <strong>Stay in the know</strong>
+        </p>
+        <p>Sign up for information on carepkg sales and deals</p>
+        <form>
+          <label htmlFor="email">
+            <strong>Email:</strong>
+          </label>
+          <input
+            id="email-sub-input"
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={e => this.handleType(e)}
+          />
+          <button className="footer-btn">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CarepkgNewsletter;
