6802798b03c41972b3819743d63bcaad6d83a135
Henry Griffith <hgriffithdev@gmail.com>
2020-04-22 16:31:50 -0700

    select state styling and cancel button refreshes state

--- src/components/AccountSettings/AddAddress.js
+++ src/components/AccountSettings/AddAddress.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const initialState = {
+  address1: "",
+  address2: "",
+  city: "",
+  stateAbbr: "",
+  country: "",
+  zip: "",
+  phone: "",
+  setAsDefault: false
+};
 class AddAddress extends React.Component {
   constructor() {
     super();
@@ -7,7 +17,7 @@ class AddAddress extends React.Component {
       address1: "",
       address2: "",
       city: "",
-      state: "",
+      stateAbbr: "",
       country: "",
       zip: "",
       phone: "",
@@ -15,6 +25,7 @@ class AddAddress extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetState = this.resetState.bind(this);
   }
   handleChange(event) {
     this.setState({
@@ -23,6 +34,9 @@ class AddAddress extends React.Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+  }
+  resetState() {
+    this.setState(initialState);
   }
   render() {
     return (
@@ -53,17 +67,76 @@ class AddAddress extends React.Component {
             placeholder="City"
             autoComplete="off"
           />
-          <input
-            name="state"
-            type="text"
-            value={this.state.state}
+          <select
+            className="add-address-select"
+            name="stateAbbr"
             onChange={this.handleChange}
-            placeholder="State"
-            autoComplete="off"
-          />
+            value={this.state.stateAbbr || "State"}
+          >
+            <option value="">Select</option>
+            <option value="AL">AL</option>
+            <option value="AK">AK</option>
+            <option value="AS">AS</option>
+            <option value="AZ">AZ</option>
+            <option value="AR">AR</option>
+            <option value="CA">CA</option>
+            <option value="CO">CO</option>
+            <option value="CT">CT</option>
+            <option value="DE">DE</option>
+            <option value="DC">DC</option>
+            <option value="FL">FL</option>
+            <option value="GA">GA</option>
+            <option value="GU">GU</option>
+            <option value="HI">HI</option>
+            <option value="ID">ID</option>
+            <option value="IL">IL</option>
+            <option value="IN">IN</option>
+            <option value="IA">IA</option>
+            <option value="KS">KS</option>
+            <option value="KY">KY</option>
+            <option value="LA">LA</option>
+            <option value="ME">ME</option>
+            <option value="MD">MD</option>
+            <option value="MH">MH</option>
+            <option value="MA">MA</option>
+            <option value="MI">MI</option>
+            <option value="FM">FM</option>
+            <option value="MN">MN</option>
+            <option value="MS">MS</option>
+            <option value="MO">M0</option>
+            <option value="MT">MT</option>
+            <option value="NE">NE</option>
+            <option value="NV">NV</option>
+            <option value="NH">NH</option>
+            <option value="NJ">NJ</option>
+            <option value="NM">NM</option>
+            <option value="NY">NY</option>
+            <option value="NC">NC</option>
+            <option value="ND">ND</option>
+            <option value="MP">MP</option>
+            <option value="OH">0H</option>
+            <option value="OK">0K</option>
+            <option value="OR">0R</option>
+            <option value="PW">PW</option>
+            <option value="PA">PA</option>
+            <option value="PR">PR</option>
+            <option value="RI">RI</option>
+            <option value="SC">SC</option>
+            <option value="SD">SD</option>
+            <option value="TN">TN</option>
+            <option value="TX">TX</option>
+            <option value="UT">UT</option>
+            <option value="VT">VT</option>
+            <option value="VA">VA</option>
+            <option value="VI">VI</option>
+            <option value="WA">WA</option>
+            <option value="WV">WV</option>
+            <option value="WI">WI</option>
+            <option value="WY">WY</option>
+          </select>
           <select
             name="country"
-            className="add-address-country-select"
+            className="add-address-select"
             value={this.state.country || "United States"}
             onChange={this.handleChange}
           >
@@ -77,7 +150,7 @@ class AddAddress extends React.Component {
             type="text"
             value={this.state.zip}
             onChange={this.handleChange}
-            placeholder="Zip Code"
+            placeholder="Zip/Postal Code"
             autoComplete="off"
           />
           <input
@@ -91,7 +164,9 @@ class AddAddress extends React.Component {
         </form>
         <div id="add-address-btn-menu">
           <button className="add-address-save-btn">Save</button>
-          <button className="add-address-cancel-btn">Cancel</button>
+          <button className="add-address-cancel-btn" onClick={this.resetState}>
+            Cancel
+          </button>
         </div>
       </div>
     );
