0db4db496db049f68973de627821f48fc96b29af
Henry Griffith <hgriffithdev@gmail.com>
2020-04-21 21:33:45 -0700

    add address form styling

--- src/components/AccountSettings/AddAddress.js
+++ src/components/AccountSettings/AddAddress.js
@@ -13,31 +13,76 @@ class AddAddress extends React.Component {
       phone: "",
       setAsDefault: false
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
     });
   }
+  handleSubmit(event) {
+    event.preventDefault();
+  }
   render() {
     return (
-      <div>
-        <form>
-          <label htmlFor></label>
-          <input name="address1" type="text" value={this.state.address1} />
-          <label htmlFor></label>
-          <input name="address2" type="text" value={this.state.address2} />
-          <label htmlFor></label>
-          <input name="city" type="text" value={this.state.city} />
-          <label htmlFor></label>
-          <input name="state" type="text" value={this.state.state} />
-          <label htmlFor></label>
-          <input name="country" type="text" value={this.state.country} />
-          <label htmlFor></label>
-          <input name="zip" type="text" value={this.state.zip} />
-          <label htmlFor></label>
-          <input name="phone" type="text" value={this.state.phone} />
+      <div id="add-address-page">
+        <h3>Add Address</h3>
+        <form id="add-address-form">
+          <input
+            name="address1"
+            type="text"
+            value={this.state.address1}
+            onChange={this.handleChange}
+            placeholder="Address"
+          />
+          <input
+            name="address2"
+            type="text"
+            value={this.state.address2}
+            onChange={this.handleChange}
+            placeholder="Address"
+          />
+          <input
+            name="city"
+            type="text"
+            value={this.state.city}
+            onChange={this.handleChange}
+            placeholder="City"
+          />
+          <input
+            name="state"
+            type="text"
+            value={this.state.state}
+            onChange={this.handleChange}
+            placeholder="State"
+          />
+          <input
+            name="country"
+            type="text"
+            value={this.state.country}
+            onChange={this.handleChange}
+            placeholder="Country"
+          />
+          <input
+            name="zip"
+            type="text"
+            value={this.state.zip}
+            onChange={this.handleChange}
+            placeholder="Zip Code"
+          />
+          <input
+            name="phone"
+            type="text"
+            value={this.state.phone}
+            onChange={this.handleChange}
+            placeholder="Phone"
+          />
         </form>
+        <div id="add-address-btn-menu">
+          <button className="add-address-save-btn">Save</button>
+          <button className="add-address-cancel-btn">Cancel</button>
+        </div>
       </div>
     );
   }
