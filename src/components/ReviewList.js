e4b196457d6a9acb0286dbdbaa9ace833cb510d8
Henry Griffith <hgriffithdev@gmail.com>
2020-04-30 14:16:50 -0700

    review statistics: overall rating, bar graph

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import ReviewCard from "./ReviewCard";
 import StarRatings from "react-star-ratings";
 
 const ReviewList = props => {
   const { reviews, product, avgRating } = props;
+  const [writing, isWriting] = useState(false);
   let avg, flooredAvg, fullstars;
   avgRating && (avg = Number(avgRating));
   avg && (flooredAvg = Math.floor(avg));
-
   flooredAvg && (fullstars = Array(flooredAvg).fill(""));
 
+  const reviewSummary = Array(5).fill(0);
+  if (reviewSummary && reviews) {
+    for (let i = 0; i < reviews.length; i++) {
+      reviewSummary[reviews[i].rating - 1]++;
+    }
+  }
+  console.log(reviews);
+  console.log(reviewSummary);
   return (
     <div id="sp-reviews-page">
       <div id="sp-reviews-page-header">
         <span id="reviews-header-text">Reviews</span>
       </div>
       <div id="sp-review-stats">
-        <StarRatings
-          // ignoreInlineStyles={true}
-          rating={avg}
-          starRatedColor="black"
-          numberOfStars={5}
-        />
-        <div></div>
+        <div className="sp-overall-rating">
+          <h3>Overall Rating</h3>
+          <h5>
+            {avg} based on {reviews && reviews.length} ratings
+          </h5>
+          <StarRatings
+            rating={avg}
+            starRatedColor="black"
+            numberOfStars={5}
+            starDimension={"32px"}
+            starEmptyColor={"rgb(150, 150, 150)"}
+          />
+        </div>
+        <div className="sp-review-summary">
+          <h3>Review Summary</h3>
+          {reviewSummary &&
+            reviews &&
+            reviewSummary.reverse().map((stat, idx) => {
+              return (
+                <div className="sp-review-statline">
+                  <span>{5 - idx} stars</span>
+                  <div
+                    style={{
+                      backgroundColor: "black",
+                      width: `${(reviewSummary[idx] / reviews.length) * 300}px`
+                    }}
+                  ></div>
+                  <div
+                    style={{
+                      backgroundColor: "rgb(150, 150, 150)",
+                      width: `${((reviews.length - reviewSummary[idx]) /
+                        reviews.length) *
+                        300}px`
+                    }}
+                  ></div>
+                  <span>{reviewSummary[idx]}</span>
+                </div>
+              );
+            })}
+        </div>
       </div>
       <div id="sp-write-review-container">
         <h1>What did you think about this product?</h1>
