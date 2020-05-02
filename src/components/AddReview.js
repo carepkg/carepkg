b4004a8104ae8436ad852f3a7727e21bbe3f530c
Henry Griffith <hgriffithdev@gmail.com>
2020-05-02 15:01:48 -0700

    added stars rating from

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -46,6 +46,53 @@ class AddReview extends React.Component {
         <div className="add-review-stars-container">
           <label>
             Rating <span>*</span>
+            <div className="txt-center">
+              <form>
+                <div className="rating">
+                  <input
+                    id="star5"
+                    name="star"
+                    type="radio"
+                    value="5"
+                    className="radio-btn hide"
+                  />
+                  <label for="star5">☆</label>
+                  <input
+                    id="star4"
+                    name="star"
+                    type="radio"
+                    value="4"
+                    className="radio-btn hide"
+                  />
+                  <label for="star4">☆</label>
+                  <input
+                    id="star3"
+                    name="star"
+                    type="radio"
+                    value="3"
+                    className="radio-btn hide"
+                  />
+                  <label for="star3">☆</label>
+                  <input
+                    id="star2"
+                    name="star"
+                    type="radio"
+                    value="2"
+                    className="radio-btn hide"
+                  />
+                  <label for="star2">☆</label>
+                  <input
+                    id="star1"
+                    name="star"
+                    type="radio"
+                    value="1"
+                    className="radio-btn hide"
+                  />
+                  <label for="star1">☆</label>
+                  <div className="clear"></div>
+                </div>
+              </form>
+            </div>
           </label>
         </div>
         <label>
