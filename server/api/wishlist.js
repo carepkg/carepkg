4b1c92f9ebf50d5ffc81943a65fa4bd4a08a6bc8
Henry Griffith <hgriffithdev@gmail.com>
2020-05-22 22:11:57 -0700

    wishlist properly shows up;

--- server/api/wishlist.js
+++ server/api/wishlist.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
 const Sequelize = require("sequelize");
-const { WishlistLineItem } = require("../db/models");
+const { WishlistLineItem, Product } = require("../db/models");
 
 router.get("/:userId", async (req, res, next) => {
   try {
     const items = await WishlistLineItem.findAll({
       where: {
         userId: req.params.userId
-      }
+      },
+      include: [
+        {
+          model: Product
+        }
+      ]
     });
     res.json(items);
   } catch (err) {
--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -7,14 +7,39 @@ const Wishlist = props => {
     props.fetchWishlist(props.userId);
   }, []);
   const { wishlist } = props;
+  console.log(wishlist);
   return wishlist ? (
     <div className="cart-items-container">
       <div className="cart-items-header-table">
         <span className="cart-header-desc">Item Description</span>
-        <span className="cart-header-edit">Edit</span>
-        <span className="cart-header-price">Each</span>
+        <span className="cart-header-edit wishlist-header-edit">Edit</span>
+        <span className="cart-header-price wishlist-header-price">Each</span>
       </div>
-      {wishlist.map(lineItem => {})}
+      {wishlist.map(lineItem => {
+        const { product } = lineItem;
+        return (
+          <div className="cart-item">
+            <div className="cart-item-desc-box">
+              <img src={product.image} style={{ width: 80, height: 80 }} />
+              <div>
+                <h4>{product.name}</h4>
+                <div className="cart-item-desc-specs">
+                  <span>Color: default</span>&nbsp;&nbsp;
+                  <span>Size: default</span>
+                </div>
+              </div>
+            </div>
+            <div className="cart-item-edit-box wishlist-item-edit-box">
+              <button className="white-btn">Move to Cart</button>
+              <button>Remove</button>
+            </div>
+            <div className="cart-item-price-box wishlist-item-price-box">
+              <span className="dollar-sign">$</span>
+              {product.price}
+            </div>
+          </div>
+        );
+      })}
       {/* need to include product for wishlist so i can map thru */}
     </div>
   ) : null;
@@ -22,6 +47,6 @@ const Wishlist = props => {
 
 const mapState = state => ({ wishlist: state.wishlist, userId: state.user.id });
 const mapDispatch = dispatch => ({
-  fetchWishlist: userId => getUserWishlistThunk(userId)
+  fetchWishlist: userId => dispatch(getUserWishlistThunk(userId))
 });
 export default connect(mapState, mapDispatch)(Wishlist);
