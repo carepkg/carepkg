8cdcb88e54bd7480ff1d0e5301deee81dfec7056
Henry Griffith <hgriffithdev@gmail.com>
2020-05-22 18:29:08 -0700

    get wishlist by user route

--- server/api/wishlist.js
+++ server/api/wishlist.js
@@ -2,4 +2,17 @@ const router = require("express").Router();
 const Sequelize = require("sequelize");
 const { WishlistLineItem } = require("../db/models");
 
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const items = await WishlistLineItem.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    });
+    res.json(items);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
