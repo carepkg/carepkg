c3fd3a51328726e1159ee6fba3963bb4c260d59a
Henry Griffith <hgriffithdev@gmail.com>
2020-11-20 13:20:55 -0800

    fixed text typos

--- src/components/About/AboutWhenSection.js
+++ src/components/About/AboutWhenSection.js
@@ -12,16 +12,17 @@ const AboutWhenSection = () => {
       </p>
       <p className="s3-text">
         At the dawn of time - one hundred years ago - a cataclysmic celestial
-        anomaly transpired that ushered in a new era of tiny electronic gadgets
-        and an unprecedented demand for outdoor equipment. At its epicenter, a
-        website manifested out not of thin air, but of an unwavering desire to
-        give everbody everything they've ever wanted. That was in 2015. Today,
-        we still dominate the industry with signature products like our Men's
-        Pro Safari Shorts to name a few. Even our cookingware keyset is doing
-        better than our competition's best selling stuff. REI. Osh. Dick's. All
-        second-place finishers quivering in their Carepkg&trade; hiking boots.
-        Too bad they didn't also buy our thermal tube socks which, if you buy in
-        the next 24 hours, are 6% off. And trust us, you'll want those 100% on.
+        event ushered in a new era of consumption, characterized by tiny
+        electronic gadgets and an unprecedented demand for outdoor equipment. At
+        its epicenter, a website manifested out not of thin air, but of an
+        unwavering desire to give everbody everything they've ever wanted. That
+        was in 2015. Today, we still dominate the industry with signature
+        products like our Men's Pro Safari Shorts to name a few. Even our
+        cookingware keyset is doing better than our competition's best selling
+        stuff. REI. Osh. Dick's. All second-placers quivering in their
+        Carepkg&trade; boots. Too bad they didn't also buy our thermal socks to
+        match which, if you buy in the next 24 hours, are 6% off. And trust us,
+        you'll want those 100% on.
       </p>
       <div>
         <Discountdown />
