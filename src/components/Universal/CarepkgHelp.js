5a0503b14ee4b198fc98a32352a6b6793f187dbf
Henry Griffith <hgriffithdev@gmail.com>
2020-11-20 13:24:14 -0800

    fixed text spacing

--- src/components/Universal/CarepkgHelp.js
+++ src/components/Universal/CarepkgHelp.js
@@ -5,7 +5,7 @@ const CarepkgHelp = () => {
     <div className="footer-snippet">
       <p className="bold footer-header">Need Help?</p>
       <p className="footer-sub-header">Chat with our support team 24/7</p>
-      <button className="medium-btn-black"> Chat Now</button>
+      <button className="medium-btn-black">Chat Now</button>
       <p className="bold" style={{ marginTop: "4px" }}>
         Or Call: 1-234-567-8910
       </p>
